dd897b8e0d787318d3116c2ed8c6b9f27c1ddb64
barry <nicolai.bernhard03@gmail.com>
2021-12-30 23:15:43 +0100

    f

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,38 +1,47 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
-import './nav.scss';
+import "./nav.scss";
 
 export const Navbar = () => {
   return (
     <nav>
       <figure>
         <Link to="/">
-            <img id="personal-logo" src={logo} alt="" />
+          <img id="personal-logo" src={logo} alt="" />
         </Link>
       </figure>
       <ul className="nav__link--list">
         <li className="nav__link">
-          <Link to="/" 
-              className="
+          <Link
+            to="/"
+            className="
               nav__link--anchor
               link__hover-effect
-              link__hover-effect--black">
-          Home</Link>
+              link__hover-effect--black"
+          >
+            Home
+          </Link>
         </li>
         <li className="nav__link">
-          <Link to="/" 
-              className="
+          <Link
+            to="/"
+            className="
               nav__link--anchor
               link__hover-effect
-              link__hover-effect--black">
-          Blog</Link>
+              link__hover-effect--black"
+          >
+            Blog
+          </Link>
         </li>
         <li className="nav__link">
-          <Link to="/" 
-              className="
+          <Link
+            to="/"
+            className="
               nav__link--anchor
-              nav__link--anchor-primary">
-          Contact</Link>
+              nav__link--anchor-primary"
+          >
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
