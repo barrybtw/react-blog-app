7b0ea5fd2c3461f3c8b8ac190d77957fd63f0115
barry <nicolai.bernhard03@gmail.com>
2021-12-30 22:24:24 +0100

    Added homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GlobalContext } from "./context/GlobalContext";
+
+import { Home } from "./pages/Home";
 export const App = () => {
   const [isAuth, setIsAuth] = useState(false);
 
@@ -8,7 +10,8 @@ export const App = () => {
     <GlobalContext.Provider value={(isAuth, setIsAuth)}>
       <Router>
         <Routes>
-          <Route path="/" element={<h1>hi</h1>} />
+          <Route path="/*" element={<Home />} />
+          <Route path="/posts" element={<h1>hi</h1>} />
         </Routes>
       </Router>
     </GlobalContext.Provider>
