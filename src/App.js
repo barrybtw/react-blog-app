b69edf4218ab681991c5862754da9553fafd03ab
barry <nicolai.bernhard03@gmail.com>
2021-12-30 22:35:26 +0100

    Added navigation bar to router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GlobalContext } from "./context/GlobalContext";
 
 import { Home } from "./pages/Home";
+import { Navbar } from "./components/Navbar";
 export const App = () => {
   const [isAuth, setIsAuth] = useState(false);
 
   return (
     <GlobalContext.Provider value={(isAuth, setIsAuth)}>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/*" element={<Home />} />
           <Route path="/posts" element={<h1>hi</h1>} />
