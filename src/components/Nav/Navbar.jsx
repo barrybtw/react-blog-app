fb552ed52a92bee7c0aad9d789f93e445599ff14
JawadH02 <JawadHanif.002@gmail.com>
2021-12-30 17:14:00 -0500

    nav

---
+++ src/components/Nav/Navbar.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import './nav.scss';
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <figure>
+        <Link to="/">
+            <img id="personal-logo" src={logo} alt="" />
+        </Link>
+      </figure>
+      <ul className="nav__link--list">
+        <li className="nav__link">
+          <Link to="/" 
+              className="
+              nav__link--anchor
+              link__hover-effect
+              link__hover-effect--black">
+          Home</Link>
+        </li>
+        <li className="nav__link">
+          <Link to="/" 
+              className="
+              nav__link--anchor
+              link__hover-effect
+              link__hover-effect--black">
+          Blog</Link>
+        </li>
+        <li className="nav__link">
+          <Link to="/" 
+              className="
+              nav__link--anchor
+              nav__link--anchor-primary">
+          Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
