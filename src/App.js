fa1424a0592e267da0e5846e3160e679ee9227a9
barry <nicolai.bernhard03@gmail.com>
2021-12-30 22:11:25 +0100

    init #1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { GlobalContext } from "./context/GlobalContext";
 export const App = () => {
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<h1>hi</h1>} />
-      </Routes>
-    </Router>
+    <GlobalContext.Provider value={(isAuth, setIsAuth)}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>hi</h1>} />
+        </Routes>
+      </Router>
+    </GlobalContext.Provider>
   );
 };
