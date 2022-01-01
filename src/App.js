0481468c3d3d32995015ab207e0957fe39c8fe3e
barry <nicolai.bernhard03@gmail.com>
2022-01-02 00:09:07 +0100

    f

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ export const App = () => {
         <Route path="/new" element={<NewBlog isAuth={isAuth} />} />
         <Route path="profile" element={<User />} />
         <Route path="user/:id" element={<Profile />} />
+        <Route path="users" element={<Usersite />} />
       </Routes>
     </Router>
   );
