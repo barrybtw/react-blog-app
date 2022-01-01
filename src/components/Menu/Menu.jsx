74ebb8d86eee59548770355b571cc2721310d3f8
JawadH02 <JawadHanif.002@gmail.com>
2022-01-01 16:08:24 -0500

    menu profile pic

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -4,25 +4,29 @@ import { CgProfile } from "react-icons/cg";
 import { auth } from "../../firebase/config";
 import { FaBeer } from "react-icons/fa";
 
-export const Menu = () => {
-    console.log(auth.currentUser.photoURL)
-    return (
-        <div className="menu__container">
-            <button className="menu__buttons" >
-                {/* <img src={ auth.currentUser.photoURL } alt="" /> */}
-            </button>
-            <div className="menu__button--list-wrapper">
-                <ul className="menu__button--list">
-                    <li className="menu__button--item">
-                        <FiSettings />
-                        <span className="menu__button--item-name"></span>
-                    </li>
-                    <li className="menu__button--item">
-                        <CgProfile />
-                        <span className="menu__button--item-name">Profile</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+export const Menu = ({ isAuth }) => {
+  let profilePhoto = auth.currentUser?.providerData[0].photoURL;
+  return (
+    <div className="menu__container">
+      <button className="menu__buttons">
+        {isAuth ? (
+          <img className="profile__photo" src={profilePhoto} alt="" />
+        ) : (
+          <FaBeer />
+        )}
+      </button>
+      <div className="menu__button--list-wrapper">
+        <ul className="menu__button--list">
+          <li className="menu__button--item">
+            <FiSettings />
+            <span className="menu__button--item-name"></span>
+          </li>
+          <li className="menu__button--item">
+            <CgProfile />
+            <span className="menu__button--item-name">Profile</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { getAuth, GoogleAuthProvider, updateProfile } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
-import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,15 +23,3 @@ export const provider = new GoogleAuthProvider();
 export const auth = getAuth();
 export const db = getFirestore();
 export const storage = getStorage(firebaseApp);
-
-export async function upload(file, currentUser) {
-  const fileRef = ref(storage, currentUser.uid);
-
-  const snapshot = await uploadBytes(fileRef, file);
-
-  const photoURL = await getDownloadURL(fileRef);
-
-  updateProfile(currentUser, { photoURL });
-
-  console.log("Uploaded file");
-}
