5325ef142b6f3512da3bb5e1c6654faa0dbf449f
barry <nicolai.bernhard03@gmail.com>
2022-01-02 00:37:13 +0100

    d

--- src/components/Userlist/Userlist.jsx
+++ src/components/Userlist/Userlist.jsx
@@ -1,7 +1,23 @@
+import { collection, getDocs, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../../firebase/config";
+
 export const Userlist = () => {
+  const [users, setUsers] = useState([]);
+  const UserCollection = query(collection(db, "users"));
+  useEffect(() => {
+    const getUsers = async () => {
+      const Users = await getDocs(query(UserCollection));
+      setUsers(Users.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, []);
+
   return (
     <div>
       <h1>fasfsfdsaddsad</h1>
+      {users && <h1>Loaded</h1>}
     </div>
   );
 };
