6bd69edd901e03c3231bbe2ac2ef3d3f09e7b035
JawadH02 <JawadHanif.002@gmail.com>
2022-01-01 16:18:26 -0500

    bb

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,12 +3,19 @@ import { FiSettings } from "react-icons/fi";
 import { CgProfile } from "react-icons/cg";
 import { auth } from "../../firebase/config";
 import { FaBeer } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const Menu = ({ isAuth }) => {
   let profilePhoto = auth.currentUser?.providerData[0].photoURL;
+  const [active, setActive] = useState(false);
+
+  const handleClick = (e) => {
+      setActive(!active)
+  }
   return (
     <div className="menu__container">
-      <button className="menu__buttons">
+      <button className="menu__buttons" onClick={handleClick}>
         {isAuth ? (
           <img className="profile__photo" src={profilePhoto} alt="" />
         ) : (
@@ -18,12 +25,16 @@ export const Menu = ({ isAuth }) => {
       <div className="menu__button--list-wrapper">
         <ul className="menu__button--list">
           <li className="menu__button--item">
-            <FiSettings />
-            <span className="menu__button--item-name"></span>
+            <Link to="/">
+              <FiSettings />
+              <span className="menu__button--item-name">Settings</span>
+            </Link>
           </li>
           <li className="menu__button--item">
-            <CgProfile />
-            <span className="menu__button--item-name">Profile</span>
+            <Link to="/">
+              <CgProfile />
+              <span className="menu__button--item-name">Profile</span>
+            </Link>
           </li>
         </ul>
       </div>
