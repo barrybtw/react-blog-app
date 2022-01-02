e53c2a5158e46b8959fd1d167d541388df7ae1b6
JawadH02 <JawadHanif.002@gmail.com>
2022-01-02 15:08:12 -0500

    updated User Profile list Page style

--- src/components/Userlist/Userlist.jsx
+++ src/components/Userlist/Userlist.jsx
@@ -30,27 +30,37 @@ export const Userlist = () => {
     setAmount(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
   };
   return (
-    <div className="users__page">
-      <div className="users__grid">
-        {users &&
-          users.length > 0 &&
-          users.map((user) => {
-            return (
-              <div
-                className="profile"
-                key={user.userID}
-                style={{ fontFamily: "Nunito" }}
-                onClick={() => {
-                  navigate(`/user/${user.userID}`);
-                }}
-              >
-                <img src={user.photoURL} alt="" className="profile__image" />
-                <div className="profile__name">{user.userName}</div>
-                <div className="profile__detail">{user.biography}</div>
-              </div>
-            );
-          })}
+    <section className="users-gallery">
+      <div className="users-gallery__container">
+        <div className="users-gallery__row">
+          <h1 className="section__header">User Profile List</h1>
+          <figure className="users-gallery__img--wrapper">
+            {users &&
+              users.length > 0 &&
+              users.map((user) => {
+                return (
+                  <>
+                    <div className="users-gallery--overlay">
+                      <img
+                        src={user.photoURL}
+                        className="users-gallery--img"
+                        alt=""
+                        key={user.userID}
+                      />
+                      <div
+                        className="users-gallery__about-me"
+                        onClick={() => navigate(`/user/${user.userID}`)}
+                      >
+                        <div className="profile__name">{user.userName}</div>
+                        <div className="profile__detail">{user.biography}</div>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+          </figure>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
