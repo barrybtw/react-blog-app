21208ed01be42b68ddb0c42bc990fcc70b58f40d
barry <nicolai.bernhard03@gmail.com>
2022-01-02 22:48:21 +0100

    FAQ Header

--- src/pages/Faq/Faq.jsx
+++ src/pages/Faq/Faq.jsx
@@ -1,13 +1,23 @@
 //React
 
 import "./faq.scss";
+import Background from "./../../assets/faq.jpg";
 
 //
 export const Faq = () => {
   return (
     <section className="faq__container">
-      <div>hi</div>
-      <div>hi</div>
+      <div className="faq__header">
+        <div className="faq__title">FREQUENTLY ASKED QUESTIONS</div>
+        <div className="faq__desc">
+          <span>
+            <stroke>Got</stroke> a question?
+          </span>
+          <span>
+            We're here to <stroke>answer!</stroke>
+          </span>
+        </div>
+      </div>
     </section>
   );
 };
