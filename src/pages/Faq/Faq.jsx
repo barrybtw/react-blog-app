33492ed45f2040b497d71d6712aaaefd7cb0cdc2
barry <nicolai.bernhard03@gmail.com>
2022-01-02 22:51:40 +0100

    f

--- src/pages/Faq/Faq.jsx
+++ src/pages/Faq/Faq.jsx
@@ -18,6 +18,40 @@ export const Faq = () => {
           </span>
         </div>
       </div>
+      <div className="faq__cards">
+        <div className="faq__card faq__card-1">
+          <h1 className="faq__card-title">QUESTION</h1>
+          <p className="faq__card-answer">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Dignissimos ab ipsa veniam odit unde voluptate dolorem soluta,
+            dolorum cumque inventore.
+          </p>
+        </div>
+        <div className="faq__card faq__card-2">
+          <h1 className="faq__card-title">QUESTION</h1>
+          <p className="faq__card-answer">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Dignissimos ab ipsa veniam odit unde voluptate dolorem soluta,
+            dolorum cumque inventore.
+          </p>
+        </div>
+        <div className="faq__card faq__card-3">
+          <h1 className="faq__card-title">QUESTION</h1>
+          <p className="faq__card-answer">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Dignissimos ab ipsa veniam odit unde voluptate dolorem soluta,
+            dolorum cumque inventore.
+          </p>
+        </div>
+        <div className="faq__card faq__card-4">
+          <h1 className="faq__card-title">QUESTION</h1>
+          <p className="faq__card-answer">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Dignissimos ab ipsa veniam odit unde voluptate dolorem soluta,
+            dolorum cumque inventore.
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
