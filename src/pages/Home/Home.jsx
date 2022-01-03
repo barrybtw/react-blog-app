df701df9d38ef57c0537ed689dba6300f0fd7078
barry <nicolai.bernhard03@gmail.com>
2022-01-03 17:27:51 +0100

    Home page edit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,6 +11,7 @@ import { auth, db } from "../../firebase/config";
 import "./custom.scss";
 import Campfire from "./../../assets/campfire.png";
 import auroraImage from "./../../assets/aurora.png";
+import aurora2 from "./../../assets/aur2.png";
 
 export const Home = ({ isAuth }) => {
   const [postLists, setPostList] = useState([]);
@@ -41,24 +42,38 @@ export const Home = ({ isAuth }) => {
             <h1 className="welcome__back">
               Welcome {auth.currentUser.displayName}
             </h1>
-            <img src={Campfire} alt="" className="welcome__back-img" />
+            <img src={Campfire} alt="Campfire" className="welcome__back-img" />
           </div>
         ) : (
           <div className="welcome__back-div">
             <h1 className="welcome__back">Welcome to Cabin</h1>
-            <img src={Campfire} alt="" className="welcome__back-img" />
+            <img src={Campfire} alt="Campfire" className="welcome__back-img" />
           </div>
         )}
         {postLists.map((post) => (
           <div className="home__blog-container">
             <div className="home__blog-aurora">
-              <img src={auroraImage} alt="" className="home__blog-aurora-img" />
-              {post.image && (
-                <img
-                  src={post.image}
-                  alt={`Image in blog of ${post.authorName}`}
-                  className="home__blog-image"
-                />
+              {post.image ? (
+                <>
+                  <img
+                    src={auroraImage}
+                    alt="Aurora effects"
+                    className="home__blog-aurora-img"
+                  />
+                  <img
+                    src={post.image}
+                    alt={`${post.authorName}' blog`}
+                    className="home__blog-image"
+                  />
+                </>
+              ) : (
+                <>
+                  <img
+                    src={aurora2}
+                    alt="Aurora effects"
+                    className="home__blog-aurora-img"
+                  />
+                </>
               )}
             </div>
 
@@ -71,60 +86,21 @@ export const Home = ({ isAuth }) => {
             <div className="home__blog-metadata">
               <p>Written by {post.authorName}</p>
             </div>
+            {isAuth && post.authorID === auth.currentUser.uid && (
+              <p
+                className="home__blog-delete"
+                onClick={() => {
+                  deletePost(post.id);
+                }}
+              >
+                DELETE
+              </p>
+            )}
           </div>
         ))}
       </div>
     );
   };
-
-  const RenderPosts = () => (
-    <div className="home-blog__list">
-      {isAuth ? (
-        <div className="welcome__back-div">
-          <h1 className="welcome__back">
-            Welcome {auth.currentUser.displayName}
-          </h1>
-          <img src={Campfire} alt="" className="welcome__back-img" />
-        </div>
-      ) : (
-        <div className="welcome__back-div">
-          <h1 className="welcome__back">Welcome to Cabin</h1>
-          <img src={Campfire} alt="" className="welcome__back-img" />
-        </div>
-      )}
-      {postLists.map((post) => {
-        return (
-          <div className="blog__container" key={post.id}>
-            <div className="blog__content">
-              <h1 className="blog__content-title">{post.title}</h1>
-              {post.image && (
-                <div className="blog__post-picture--mask">
-                  <img className="home__post--img" src={post.image} alt="" />
-                </div>
-              )}
-              <div className="postTextContainer">{post.post} </div>
-              <div className="postMetaInfo">
-                <h6 className="blog__author">@ {post.authorName}</h6>
-                <h6>Posted {post.postTime}</h6>
-                {isAuth && post.authorID === auth.currentUser.uid && (
-                  <div className="deletePost">
-                    <button
-                      className="noselect"
-                      onClick={() => {
-                        deletePost(post.id);
-                      }}
-                    >
-                      <div></div>
-                    </button>
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
 
   return (
     <div className="home__container">
